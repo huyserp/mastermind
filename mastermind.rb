95bb8039c94090ba11dfcc4d0d0c5b867c106c23
Peter Huyser <huyserp@gmail.com>
2020-06-07 13:47:39 +0200

    working game with UI for human to guess

--- mastermind.rb
+++ mastermind.rb
@@ -5,10 +5,6 @@ class String
         "\e[#{color_code}m#{self}\e[0m"
     end
 
-    def bold
-        "\e[1m#{self}\e[22m"
-    end
-
     def red
         colorize(31)
     end
@@ -37,12 +33,13 @@ class String
         colorize(42)
     end
 
-
-
+    def bold
+        "\e[1m#{self}\e[22m"
+    end
 end
 
 class Mastermind < String
-    attr_reader :submitted_guess
+    attr_accessor :turn_count
 
     def initialize
         @red = "*".red.bold
@@ -54,16 +51,57 @@ class Mastermind < String
         @color_and_space = "  ".bg_green
 
         @code = []
-        @all_guesses = []
+        @guess_list = []
         @submitted_guess = []
-        @all_clues = []
-        @clue = [' ', ' ', ' ', ' ', ' ']
+        @clue = ['  ', '  ', '  ', '  ', '  ']
         @choices = [@red, @blue, @green, @yellow, @pink]
+        @turn_count = 1
+        @winning_statement = [
+            "Congrats, you did it! You're pretty smart.",
+            "Wow, how did you do that so fast? It only took you #{self.turn_count} tries!}",
+            "OK, ok, why don't you give someone else a turn hotshot...",
+            "You're not as dumb as you look.",
+            "Maybe try a harder difficulty? just sayin..."
+        ]
     end
 
     def play
-
+        puts "Would you like to 'SET' the code or 'GUESS' the code?"
+        game_type = gets.chomp.upcase
+        if game_type == "GUESS"
+            cpu_generate_code
+            puts "The code has been set."
+            sleep 0.5
+            while game_over? == false
+                sleep 1.5
+                pick_colors
+                check_enter_clear
+                self.turn_count += 1
+            end
+            puts @winning_statement.shuffle.first
+            puts "the code was:"
+            show_code
+            sleep 2
+            puts "Want to play again? yes/no"
+            play_again = gets.chomp.downcase
+            if play_again == "yes"
+                initialize
+                self.play
+            else
+                puts("Goodbye!")
+            end
+        elsif game_type == "SET"
+            puts "functionality coming soon... :)"
+            sleep 1
+            self.play
+        else
+            puts "yeah, ok... SET or GUESS?"
+            sleep 1
+            self.play
+        end
     end
+
+    private
 
     def cpu_generate_code
         5.times do
@@ -104,7 +142,8 @@ class Mastermind < String
         @code.each do |color|
             code_color_count[color] = @code.count(color)
         end
-
+        
+        #### SET THE GREENS FIRST ####
         @submitted_guess.each_with_index do |my_color, index|
             if my_color == @code[index]
                 @clue[index] = @color_and_space
@@ -113,8 +152,9 @@ class Mastermind < String
             end
         end
 
+        #### SET THE BLUES AFTER ####
         @submitted_guess.each_with_index do |my_color, index|
-            if @clue[index] == " " && code_color_count.keys.include?(my_color)
+            if @clue[index] == "  " && code_color_count.keys.include?(my_color)
                 @clue[index] = @color_but_space
                 code_color_count[my_color] -= 1
                 code_color_count.select! { |key, value| value > 0 }
@@ -123,16 +163,22 @@ class Mastermind < String
     end
 
     def enter_guess
-        @submitted_guess = "#{@submitted_guess.join('   ')}     | #{@clue.join(' | ')} |"
-        @all_guesses << @submitted_guess
-        @all_guesses.each do |row|
+        guess = "#{self.turn_count}. #{@submitted_guess.join('   ')}     | #{@clue.join(' | ')} |"
+        @guess_list << guess
+        @guess_list.each do |row|
            puts puts row
         end
     end
 
+    def game_over?
+        @submitted_guess == @code ? true : false
+    end
+
     def clear
-        @submitted_guess = []
-        @clue = [" ", " ", " ", " ", " "]
+        if game_over? == false
+            @submitted_guess = []
+            @clue = ["  ", "  ", "  ", "  ", "  "]
+        end
     end
 
     def check_enter_clear
@@ -144,18 +190,10 @@ class Mastermind < String
     def show_code
        puts @code.join('   ')
     end
-
-    def show_board
-    end
-
-
-    # def show_choices
-    #    @choices.join('   ')
-    # end
-
 end
 
-
+game = Mastermind.new
+game.play
 
 
 
